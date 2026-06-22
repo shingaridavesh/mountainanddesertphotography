6e5e9d08e72facb3ba59008233a12305a215f1de
Davesh Shingari <shingaridavesh@gmail.com>
2026-06-22 08:50:32 -0700

    Add slideshow

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,21 @@
       if (!name) { alert('Please enter your name.'); return; }
       alert('Thank you — your inquiry has been sent. I\'ll be in touch shortly.');
     }
+
+const slides =
+  document.querySelectorAll(".hero-slide");
+
+let currentSlide = 0;
+
+setInterval(() => {
+
+  slides[currentSlide]
+    .classList.remove("active");
+
+  currentSlide =
+    (currentSlide + 1) % slides.length;
+
+  slides[currentSlide]
+    .classList.add("active");
+
+}, 8000);
